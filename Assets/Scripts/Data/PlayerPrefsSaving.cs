dc48c88596e6a267221a2670e24ea370e6dcd46a
A00827301 <A00827301@itesm.mx>
2022-10-24 13:27:27 -0500

    Actualizacion del Slider de sonido
    Los métodos de carga y guardado ya funcionan, solo falta arreglar el UI

--- Assets/Scripts/Data/PlayerPrefsSaving.cs
+++ Assets/Scripts/Data/PlayerPrefsSaving.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class PlayerPrefsSaving : MonoBehaviour
 {
     private PlayerData playerData;
+    public Slider Test;
+    //[SerializeField] private Slider _slider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,23 +23,25 @@ public class PlayerPrefsSaving : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.S))//Cambiar este para que se guarden los valores
             SaveData();
 
-        if (Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.L))//Cambiar este para que se carguen los valores
             LoadData();
     }
 
     public void SaveData() {
-        //PlayerPrefs.SetFloat("sliderVolume", GameObject.GetComponent.("VolumeSlider<Slider>").value());
+        Test = GameObject.Find("UICanvas/PauseMenu/VolumeSlider").GetComponent<Slider>();
+        PlayerPrefs.SetFloat("sliderVolume", Test.value);
         //PlayerPrefs.SetFloat("health", playerData.health);
-
     } 
 
     public void LoadData() 
     {
-        playerData = new PlayerData(PlayerPrefs.GetFloat("sliderVolume"));
+        Test = GameObject.Find("UICanvas/PauseMenu/VolumeSlider").GetComponent<Slider>();
+        Test.value = PlayerPrefs.GetFloat("sliderVolume");
+        //playerData = new PlayerData(PlayerPrefs.GetFloat("sliderVolume"));
 
-        Debug.Log(playerData.ToString());
+        //Debug.Log(playerData.ToString());
     }
 }
