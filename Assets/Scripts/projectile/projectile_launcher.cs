9a9cec6a991b2c57c5a6f39ca839dc43b4ad7fb5
JIQA-24 <70869269+JIQA-24@users.noreply.github.com>
2022-10-27 19:49:15 -0500

    Ataque de plumas fix
    arreglado que las plumas antes movian al jugador, y las zonas de detect para activar las plumas ya no cuentan como plataformas

--- Assets/Scripts/projectile/projectile_launcher.cs
+++ Assets/Scripts/projectile/projectile_launcher.cs
@@ -5,34 +5,37 @@ using UnityEngine;
 public class projectile_launcher : MonoBehaviour
 {
     Animator animator;
-
     public Animator Boss;
-    
-    //projectile to spawn
-    public GameObject projectile;
-
-    //where to spawn the projectile
+    public GameObject projectile;//projectile to spawn
+    //public Transform[] spawnLocation;//where to spawn the projectile
+    int Zone;
     public Transform spawnLocation;
-
-    //rotation of projectile on Spawn
-    public Quaternion spawnRotation;
-
+    public Quaternion spawnRotation;//rotation of projectile on Spawn
     public float spawnTime = 0.5f;
-
     private float timeSinceSpawned = 0f;
+    public DetectionZone detectionZone;
 
     // Start is called before the first frame update
     void Start()
     {
+        Zone = 0;
+        //transform.LookAt(spawnLocation[Zone].position);   
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(detectionZone.detectedObjs.Count > 0){
+            Boss.SetBool("Feather", true);
+        }
+        else{
+            Boss.SetBool("Feather", false);
+        }
         timeSinceSpawned += Time.deltaTime;
 
         if((Boss.GetBool("Feather")) == true){
             if(timeSinceSpawned >= spawnTime) {
+                //Instantiate(projectile, spawnLocation[Zone].position, spawnRotation);
                 Instantiate(projectile, spawnLocation.position, spawnRotation);
                 timeSinceSpawned = 0f;
             }
