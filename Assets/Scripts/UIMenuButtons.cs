660516a0deb45683dda5b64224fea40d407a0b46
Vale_Cabanas <44687344+ValeCabanas@users.noreply.github.com>
2022-11-25 23:29:23 -0600

    UIX COMPLETO
    Se agrega navegación completa y se amalgaman botones para lucir igual, se agrega funcionalidad de fullscreen y pantalla de carga e inicio

--- Assets/Scripts/UIMenuButtons.cs
+++ Assets/Scripts/UIMenuButtons.cs
@@ -2,15 +2,94 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+
 
 public class UIMenuButtons : MonoBehaviour
 {
     [SerializeField] private PlayerPrefsSaving PrefsSaving;
+    [SerializeField] GameObject startMenu;
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject codeMenu;
+    [SerializeField] GameObject optionsMenu;
+    [SerializeField] GameObject codeDeploy;
+    public GameObject mainMenuStartButton;
+    public GameObject inputTextFieldCodeMenu;
+    public GameObject volumeSlider;
+    public GameObject copyCodeButton;
+    private bool menuActive;
+
+    private void Start()
+    {
+        startMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        codeMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        codeDeploy.SetActive(false);
+        menuActive = false;
+        //EnterMainMenu();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Submit")&&(!menuActive))
+        {
+            EnterMainMenu();
+            menuActive = true;
+        }
+    }
+
+    public void EnterMainMenu()
+    {
+        startMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        codeMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        codeDeploy.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(mainMenuStartButton);
+    }
+
+    public void EnterCodeMenu()
+    {
+        startMenu.SetActive(false);
+        mainMenu.SetActive(false);
+        codeMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        codeDeploy.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(inputTextFieldCodeMenu);
+    }
+
+    public void EnterOptionsMenu()
+    {
+        startMenu.SetActive(false);
+        mainMenu.SetActive(false);
+        codeMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        codeDeploy.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(volumeSlider);
+    }
+
+    public void CodeDeployMenu()
+    {
+        startMenu.SetActive(false);
+        mainMenu.SetActive(false);
+        codeMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        codeDeploy.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(copyCodeButton);
+    }
+    
     public void StartGame()
     {
         PrefsSaving.SaveData();
-        SceneManager.LoadScene("Prototype");
+        SceneManager.LoadScene("SceneCharger");
     }
+
     public void StartTutorial()
     {
         PrefsSaving.SaveData();
@@ -21,4 +100,19 @@ public class UIMenuButtons : MonoBehaviour
         PrefsSaving.SaveData();
         Application.Quit();
     }
+
+    public void FullScreenMode()
+    {
+        if(Screen.fullScreen)
+        {
+            // Toggle fullscreen
+            Screen.fullScreen = false;
+            Debug.Log("Fullscreen desactivado UwU");
+        } else 
+        {
+            Screen.fullScreen = true;
+            Debug.Log("Fullscreen activado UwU");
+        }
+    }
+
 }
